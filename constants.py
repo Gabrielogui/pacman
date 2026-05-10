0c50a5c12df549650cfb0ca479e7172ffdf820b4
Gabrielogui <gabrielcsro@gmail.com>
2026-05-10 09:29:30 -0300

    feat: constantes utilizadas na main

---
+++ constants.py
@@ -0,0 +1,26 @@
+
+# DIMENSÕES DA TELA
+LARGURA = 480
+ALTURA  = 600 
+
+# TÍTULO DO JOGO
+TITULO_JOGO = "Pacman"
+
+# FPS
+FPS = 30
+
+# CORES
+PRETO   = (0, 0, 0)
+BRANCO  = (255, 255, 255)
+AMARELO = (244, 233, 51)
+
+# IMAGENS
+SPRITESHEET        = "sprite_sheet.png"
+PACKMAN_START_LOGO = "pacman-logo-1.png"
+
+# FONTE
+FONTE = "arial"
+
+# AUDIOS
+MUSICA_START = "intermission.wav"
+TECLA_START  = "eat_dot_0.wav"
